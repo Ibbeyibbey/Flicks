20566e98ec36abef1d70d9093a8aacac09252b6d
Isha B <Girllovestech93@gmail.com>
2020-10-10 20:55:04 -0700

    Assignment2

---
+++ Flicks/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flicks
+//
+//  Created by Isha Brown on 10/10/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
